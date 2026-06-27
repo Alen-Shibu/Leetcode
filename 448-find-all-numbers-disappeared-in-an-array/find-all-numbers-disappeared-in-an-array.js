3906d7c973152fbe7e7c35e7ec75a5a6206af328
Alen Shibu <webwizlab@gmail.com>
2026-06-27 16:17:12 +0530

    Time: 22 ms (37.68%) | Memory: 77.2 MB (15.65%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    const set = new Set(nums)
+    const res = []
+
+    for(let i=1;i<nums.length+1;i++){
+        if(set.has(i)) continue;
+        res.push(i)
+    }
+    return res;
+};
