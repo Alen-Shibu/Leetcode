340da0deb2452c79f247bb0fa9fe0d18ba9ee041
Alen Shibu <webwizlab@gmail.com>
2026-07-02 06:29:44 +0530

    Time: 1 ms (76.14%) | Memory: 59 MB (84.54%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -3,20 +3,13 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let l=0;
-    let r=0;
+    let l = 0;
 
-    while(r<nums.length){
+    for(r=0;r<nums.length;r++){
         if(nums[r] !== 0){
-            temp = nums[l];
-            nums[l] = nums[r];
-            nums[r] = temp;
-
-            r++
-            l++
-        }
-        else{
-            r++
+            [nums[l],nums[r]] = [nums[r],nums[l]]
+            l++;
         }
     }
+    return nums;
 };
