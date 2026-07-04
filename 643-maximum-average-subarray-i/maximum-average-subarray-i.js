4ee9d685768c16ef8800f901b636e48d2ab50174
Alen Shibu <webwizlab@gmail.com>
2026-07-04 18:40:25 +0530

    Time: 2 ms (82.80%) | Memory: 68.9 MB (58.94%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.js
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.js
@@ -4,21 +4,16 @@
  * @return {number}
  */
 var findMaxAverage = function(nums, k) {
-    if(nums.length===1) return nums[0]
-    let max_avg = -Infinity;
-    let l = 0;
-    let r = k-1;
+    let sum = 0;
 
-    while(r<nums.length){
-        let sum = 0;
-        for(let i=l;i<r+1;i++){
-            sum += nums[i] 
-        }
-        const avg = sum/k;
-        max_avg = Math.max(avg,max_avg)
-        l++;
-        r++;
-    }
+    for(let i=0;i<k;i++) sum+= nums[i];
 
-    return max_avg;
+    let max_sum = sum;
+
+    for(let i=k;i<nums.length;i++){
+        sum += nums[i] - nums[i-k];
+        max_sum = Math.max(sum,max_sum)
+    }    
+
+    return max_sum/k;
 };
