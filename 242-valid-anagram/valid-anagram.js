cdd05635882d6bc0b314c3d818596c86867b980b
Alen Shibu <webwizlab@gmail.com>
2026-07-11 17:06:36 +0530

    Time: 9 ms (86.82%) | Memory: 55.9 MB (72.61%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -5,17 +5,12 @@
  */
 var isAnagram = function(s, t) {
     if(s.length !== t.length) return false;
-    const a1 = new Array(26).fill(0)
-    const a2 = new Array(26).fill(0)
+    const freq = new Array(26).fill(0)
 
     for(let i=0;i<s.length;i++){
-        a1[s.charCodeAt(i) - 97]++
-        a2[t.charCodeAt(i) - 97]++
+        freq[s.charCodeAt(i) - 97]++
+        freq[t.charCodeAt(i) - 97]--
     };
 
-    for(let i=0;i<26;i++){
-        if(a1[i] !== a2[i]) return false;
-    }
-
-    return true
+    return freq.every(val => val===0)
 };
