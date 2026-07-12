a6ee719483f31e25930ba1345f75cec6587ba810
Alen Shibu <webwizlab@gmail.com>
2026-07-12 23:29:44 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (24.24%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,10 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map = new Map();
+    const map = new Map();
+
     for(let i=0;i<nums.length;i++){
-        const diff = target - nums[i];
-        if(map.has(diff)) return [i,map.get(diff)];
-        map.set(nums[i],i);
+        const needed = target - nums[i]
+        if(map.has(needed)) return [map.get(needed),i]
+        map.set(nums[i],i)
     }
+
 };
